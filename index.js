558f1742b39d5bc9842c39a4ce4aeb5204f08009
1929240 <saptak.mystery@gmail.com>
2021-07-10 11:18:43 +0530

    complete working time-table

---
+++ index.js
@@ -0,0 +1,9 @@
+$(".SE").attr("href", "http://meet.google.com/zca-hgss-qos"); //legit
+$(".CN").attr("href", "https://meet.google.com/lookup/cn7qapcsoa"); //legit
+$(".DAA").attr("href", "http://meet.google.com/nsk-kawh-zgv");//legit
+$(".ITC").attr("href", "https://meet.google.com/zkn-kubc-enj");//legit
+$(".DBMS").attr("href", "http://meet.google.com/kad-tzjp-sek"); //legit
+$(".CRY").attr("href", "https://meet.google.com/lookup/d7ghplms6c"); //legit
+$(".DBMS-LAB").attr("href", "http://meet.google.com/kad-tzjp-sek");
+$(".ALGO-LAB").attr("href", "http://meet.google.com/nsk-kawh-zgv");//legit
+$(".CN-LAB").attr("href", "https://meet.google.com/lookup/cn7qapcsoa");//legit
---
+++ readme.txt
@@ -0,0 +1,2 @@
+you can have your own customised time table by modifying the text
+inside your respective subject's url in the index.js file . HAVE FUN !
