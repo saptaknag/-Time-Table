b641919d8e6ab2f523220a60a25965351da784f3
1929240 <saptak.mystery@gmail.com>
2021-07-11 09:19:33 +0530

    highlight current day and ongoing class

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-$(".SE").attr("href", "http://meet.google.com/zca-hgss-qos"); //legit
-$(".CN").attr("href", "https://meet.google.com/lookup/cn7qapcsoa"); //legit
-$(".DAA").attr("href", "http://meet.google.com/nsk-kawh-zgv");//legit
-$(".ITC").attr("href", "https://meet.google.com/zkn-kubc-enj");//legit
-$(".DBMS").attr("href", "http://meet.google.com/kad-tzjp-sek"); //legit
-$(".CRY").attr("href", "https://meet.google.com/lookup/d7ghplms6c"); //legit
+$(".SE").attr("href", "http://meet.google.com/zca-hgss-qos"); 
+$(".CN").attr("href", "https://meet.google.com/lookup/cn7qapcsoa"); 
+$(".DAA").attr("href", "http://meet.google.com/nsk-kawh-zgv");
+$(".ITC").attr("href", "https://meet.google.com/zkn-kubc-enj");
+$(".DBMS").attr("href", "http://meet.google.com/kad-tzjp-sek"); 
+$(".CRY").attr("href", "https://meet.google.com/lookup/d7ghplms6c"); 
 $(".DBMS-LAB").attr("href", "http://meet.google.com/kad-tzjp-sek");
-$(".ALGO-LAB").attr("href", "http://meet.google.com/nsk-kawh-zgv");//legit
-$(".CN-LAB").attr("href", "https://meet.google.com/lookup/cn7qapcsoa");//legit
+$(".ALGO-LAB").attr("href", "http://meet.google.com/nsk-kawh-zgv");
+$(".CN-LAB").attr("href", "https://meet.google.com/lookup/cn7qapcsoa");
+
+const date = new Date();
+var hr=date.getHours();
+const row=date.getDay();
+const col=hr-8; 
+
+if(row>=0 && row<5)
+{
+    if(row>0)
+    $("tbody tr")[row-1].classList.add("day");
+    if(col>0 && col<10)
+    {
+        if($("tbody tr")[row-1].children[col].firstChild!="")
+        {
+            $("tbody tr")[row-1].children[col].classList.add("time");
+        }
+    }
+}
--- readme.txt
+++ readme.txt
@@ -1,2 +1,8 @@
 you can have your own customised time table by modifying the text
 inside your respective subject's url in the index.js file . HAVE FUN !
+
+upcoming updates :
+
+-> highlight current day and class
+
+-> alert if entering wrong class
